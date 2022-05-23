09f7efea652668c2dd3c59e9c17703bcea2dc2f9
ThanhSonPhamFPT <102415394+ThanhSonPhamFPT@users.noreply.github.com>
2022-05-23 10:00:35 +0700

    Add files via upload

--- Object_Class/Program.cs
+++ Object_Class/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Globalization;
 using System.Numerics;
 
@@ -34,7 +35,7 @@ namespace DemoClassObject
             //Console.WriteLine(dice1.Roll());
             //Dice dice2 = new Dice(2);
             //Console.WriteLine(dice2.Roll());
-            Lab2();
+            Lab3();
         }
         static void Lab1()
         {
@@ -51,6 +52,92 @@ namespace DemoClassObject
             bankAccount.Withdraw(10);
             Console.WriteLine(bankAccount);
 
+        }
+        static void Lab3()
+        {
+            Dictionary<int, BankAccount> accounts = new Dictionary<int, BankAccount>();
+            do
+            {
+                string command = Console.ReadLine();
+                var cmdArgs = command.Split();
+                switch (cmdArgs[0])
+                {
+                    case "Create":
+                        Create(cmdArgs, accounts);
+                        break;
+                    case "Deposit":
+                        Deposit(cmdArgs, accounts);
+                        break;
+                    case "Withdraw":
+                        Withdraw(cmdArgs, accounts);
+                        break;
+                    case "Print":
+                        Print(cmdArgs, accounts);
+                        break;
+                }
+                if (cmdArgs[0] == "End")
+                {
+                    break;
+                }
+            } while (true);
+        }
+        private static void Print(string[] cmdArgs, Dictionary<int, BankAccount> accounts)
+        {
+            var id = int.Parse(cmdArgs[1]);
+            if (!accounts.ContainsKey(id))
+            {
+                Console.WriteLine("Accound does not exist");
+            }
+            else
+            {
+                var acc = accounts[id];
+                Console.WriteLine(acc);
+            }
+        }
+
+        private static void Withdraw(string[] cmdArgs, Dictionary<int, BankAccount> accounts)
+        {
+            var id = int.Parse(cmdArgs[1]);
+            if (!accounts.ContainsKey(id))
+            {
+                Console.WriteLine("Accound does not exist");
+            }
+            else
+            {
+                var acc = accounts[id];
+                var amount = decimal.Parse(cmdArgs[2]);
+                acc.Withdraw(amount);
+            }
+        }
+
+        private static void Deposit(string[] cmdArgs, Dictionary<int, BankAccount> accounts)
+        {
+            var id = int.Parse(cmdArgs[1]);
+            if (!accounts.ContainsKey(id))
+            {
+                Console.WriteLine("Accound does not exist");
+            }
+            else
+            {
+                var acc = accounts[id];
+                var amount = decimal.Parse(cmdArgs[2]);
+                acc.Deposit(amount);
+            }
+        }
+
+        private static void Create(string[] cmdArgs, Dictionary<int, BankAccount> accounts)
+        {
+            var id = int.Parse(cmdArgs[1]);
+            if (accounts.ContainsKey(id))
+            {
+                Console.WriteLine("Accound already exists");
+            }
+            else
+            {
+                var acc = new BankAccount();
+                acc.Id = id;
+                accounts.Add(id, acc);
+            }
         }
     }
 }
