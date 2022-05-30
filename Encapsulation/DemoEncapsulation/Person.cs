83f6d77618232583322736b491d57b381273a0a8
ThanhSonPhamFPT <102415394+ThanhSonPhamFPT@users.noreply.github.com>
2022-05-30 08:31:18 +0700

    Add files via upload

---
+++ Encapsulation/DemoEncapsulation/Person.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoEncapsulation
+{
+    internal class Person
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+        private decimal salary;
+        public string FirstName { get { return firstName; } set { firstName = value; } }
+        public string LastName { get { return lastName; } set { lastName = value; } }
+        public int Age { get { return age; } set { age = value; } }
+        public decimal Salary { 
+            get { return salary; } 
+            set 
+            {
+                if (value < 460)
+                {
+                    throw new ArgumentException("Salary cannot be less than 460 leva!");
+                }
+                salary = value; 
+            } 
+        }
+
+        public Person(string firstName,string lastName, int age, decimal salary)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+            this.Salary = salary;
+        }
+        //public Person(string firstName, string lastName, int age):this(firstName,lastName, age, 0)
+        //{
+
+        //}
+        public override string ToString()
+        {
+            return $"{this.firstName} {this.lastName} receives {this.salary:0.00} leva.";
+        }
+        public void IncreaseSalary(decimal percentage)
+        {
+            if (age > 30)
+            {
+                salary += salary * percentage/100;
+            }
+            else 
+            {
+                salary += salary * percentage / 200;
+            }
+        }
+    }
+}
--- Encapsulation/DemoEncapsulation/Program.cs
+++ Encapsulation/DemoEncapsulation/Program.cs
@@ -1,1 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
+namespace DemoEncapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Lab2();
+        }
+        static void Lab1()
+        {
+            //try
+            //{
+            //    var lines = int.Parse(Console.ReadLine());
+            //    var persons = new List<Person>();
+            //    for (int i = 0; i < lines; i++)
+            //    {
+            //        var cmdArgs = Console.ReadLine().Split();
+            //        var person = new Person(cmdArgs[0], cmdArgs[1], int.Parse(cmdArgs[2]));
+            //        persons.Add(person);
+            //    }
+            //    persons.OrderBy(p => p.FirstName)
+            //       .ThenBy(p => p.Age)
+            //       .ToList()
+            //       .ForEach(p => Console.WriteLine(p.ToString()));
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //}
+
+            
+        }
+
+        static void Lab2()
+        {
+
+            var lines = int.Parse(Console.ReadLine());
+            var persons = new List<Person>();
+            for (int i = 0; i < lines; i++)
+            {
+                var cmdArgs = Console.ReadLine().Split();
+                // Console.WriteLine(cmdArgs[3]);
+                try
+                {
+                    var person = new Person(cmdArgs[0],
+                                            cmdArgs[1],
+                                            int.Parse(cmdArgs[2]),
+                                            decimal.Parse(cmdArgs[3]));
+
+                    persons.Add(person);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    //   Console.WriteLine(ex.StackTrace);
+                }
+            }
+            var bonus = decimal.Parse(Console.ReadLine());
+            persons.ForEach(p => p.IncreaseSalary(bonus));
+            persons.ForEach(p => Console.WriteLine(p.ToString()));
+        }
+        
+    }
+}
