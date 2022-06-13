a42e55f1533b05abe21b05367038220900be7bfb
ThanhSonPhamFPT <102415394+ThanhSonPhamFPT@users.noreply.github.com>
2022-06-13 09:14:06 +0700

    Add files via upload

---
+++ Polymorphism/Lab/Animal.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    internal class Animal
+    {
+        public Animal(string name, string favouriteFood)
+        {
+            Name = name;
+            FavouriteFood = favouriteFood;
+        }
+
+        public string Name { get; private set; }
+        public string FavouriteFood { get; private set; }
+        public virtual string ExplainSelf()
+        {
+            return string.Format(
+              "I am {0} and my favourite food is {1}",
+              this.Name,
+              this.FavouriteFood);
+        }
+
+    }
+}
--- Polymorphism/Lab/Program.cs
+++ Polymorphism/Lab/Program.cs
@@ -1,1 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
 
+namespace Polymorphism
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Lab3();
+        }
+        static void Lab1()
+        {
+            MathOperation mo = new MathOperation();
+            Console.WriteLine(mo.Add(2, 3));
+            Console.WriteLine(mo.Add(2.2, 3.3, 5.5));
+            Console.WriteLine(mo.Add(2.2m, 3.3m, 4.4m));
+
+        }
+        static void Lab2()
+        {
+            Animal cat = new Cat("Pesho", "Whiskas");
+            Animal dog = new Dog("Gosho", "Meat");
+
+            Console.WriteLine(cat.ExplainSelf());
+            Console.WriteLine(dog.ExplainSelf());
+
+        }
+        static void Lab3()
+        {
+            var shapes = new List<Shape>();
+
+            shapes.Add(new Circle(3.5));
+            shapes.Add(new Rectangle(3.5, 1.2));
+            shapes.Add(new Rectangle(1.5, 1.5));
+            shapes.Add(new Rectangle(3.4, 1.5));
+            shapes.Add(new Circle(3.6));
+
+            foreach (var shape in shapes)
+            {
+                Console.WriteLine(shape.Draw());
+            }
+
+        }
+    }
+}
